ea5a6de675fa35cf39524c5c84b6105801a95104
AECohn0210 <AECohn0210@Gmail.com>
2023-02-02 23:24:01 -0500

    Sanitizing dragged folders

--- MainStageSampleFormatter/Program.cs
+++ MainStageSampleFormatter/Program.cs
@@ -3,10 +3,13 @@
 //This app sorts MainStage Auto Sampler samples by date created, then renames them in a way that makes them easier to load into WaveState Sample Builder
 
 Console.WriteLine("Please drag in the sample folder");
-var sampleFolder = Console.ReadLine();
+var sampleFolder = Console.ReadLine()?.Replace(@"\", @"").Trim();
+Console.WriteLine(sampleFolder);
 var folderName = Path.GetFileName(sampleFolder);
 Console.WriteLine("Please drag in the output folder");
-var outputFolder = Console.ReadLine();
+var outputFolder = (Console.ReadLine()?.Replace(@"\", @"").Trim());
+Console.WriteLine(outputFolder);
+
 
 //Get all the files in the sample folder
 if (sampleFolder != null)
@@ -19,9 +22,12 @@ if (sampleFolder != null)
     for(int i =0; i < files.Length; i++)
     {
         //Create the new file path
-        string newFilePath = Path.Combine(outputFolder, $"{folderName} {i}.wav" );
-        //Copy the file to the new location
-        File.Copy(files[i], newFilePath);
-        Console.WriteLine($"succesfully created {newFilePath}");
+        if (outputFolder != null)
+        {
+            string newFilePath = Path.Combine(outputFolder, $"{folderName} {i}.wav" );
+            //Copy the file to the new location
+            File.Copy(files[i], newFilePath);
+            Console.WriteLine($"succesfully created {newFilePath}");
+        }
     }
 }
